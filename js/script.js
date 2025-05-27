6489b2d10397cd3420640fd840a9b1dce0c33ce6
Devin-boucher <devin.boucher@stu.ocsb.ca>
2025-05-27 22:07:55 +0000

    fixed spelling

--- js/script.js
+++ js/script.js
@@ -4,35 +4,42 @@
 // Created on: April 2025
 // This file contains the JS functions for index.html
 
+
 'use strict'
+
 
 // eslint-disable-next-line no-unused-vars
 
+
 function sumDivisibleBy3or5() {
-  let numberInput = document.getElementById('userNumber').value
-  let number = parseInt(numberInput)
-  
+  let numberInput = parseInt(document.getElementById('userNumber').value)
+
   let sum = 0
   let counter = 1
 
-  while (counter <= number) {
+
+  while (counter <= numberInput ) {
     let temp3 = counter
     while (temp3 >= 3) {
       temp3 = temp3 - 3
     }
+
 
     let temp5 = counter
     while (temp5 >= 5) {
       temp5 = temp5 - 5
     }
 
+
     if ((temp3 === 0) || (temp5 === 0)) {
       sum = sum + counter
     }
 
+
     counter = counter + 1
   }
 
+
   document.getElementById('sum-result').innerText =
-    'Sum of numbers divisible by 3 or 5 up to ' + number + ' is: ' + sum
+    'Sum of numbers divisible by 3 or 5 up to ' + numberInput + ' is: ' + sum
 }
